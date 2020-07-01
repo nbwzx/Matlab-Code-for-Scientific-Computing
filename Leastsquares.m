991659cd346a08734c208b223bead92a752679ad
nbwzx <55343466+nbwzx@users.noreply.github.com>
2020-07-01 19:18:12 +0800

    Polynomial fitting by least squares

--- Leastsquares.m
+++ Leastsquares.m
@@ -1,4 +1,4 @@
-function cc=Leastsquares(x,y,k,w)
+function cc=Leastsquares(x,y,n,w)
 if nargin==3
     w=ones(1,length(x));
 elseif nargin<3
@@ -6,29 +6,29 @@ elseif nargin<3
     return
 end  
 if(length(x) == length(y))
-    n = length(x);    
+    k = length(x);    
 else
     disp('x和y的维数不相等！');
     return;
 end                                      %检错
-k=k+1;
-T=zeros(k,k);
-b=zeros(k,1);
-s=[ones(1,n)];
-for i=1:(n-1)
+n=n+1;
+T=zeros(n,n);
+b=zeros(n,1);
+s=[ones(1,k)];
+for i=1:(k-1)
     s=[s;x.^i];
 end
-for i=1:k
-    for j=1:k
+for i=1:n
+    for j=1:n
     T(i,j)=inprod(s([i],:),s([j],:),w);
     end
 end
-for i=1:k
+for i=1:n
 b(i)=inprod(s([i],:),y,w);
 end
 T
 b
-cc=T\b  
+cc=T\b;
 function ip=inprod(w,f,g)
 % 计算f和g在权w之下的内积
 ip=0;
@@ -36,11 +36,11 @@ for i=1:length(f)
 ip=ip+w(i)*f(i)*g(i);
 end
 
-%以下为输入示例：(想要多项式拟合以下数据)
+%以下为输入示例：(想要用多项式最小二乘拟合以下数据)
 %x=[0.0 0.1 0.2 0.3 0.5 0.8 1.0]
 %y=[1.0 0.41 0.50 0.61 0.91 2.02 2.46]
 %n=3
-%输入：[cc,error] = polynomialfit(x,y,k)
+%输入：cc = Leastsquares(x,y,n)
 %输出：cc =[0.9266;-4.6591;12.8147;-6.6221](代表y=0.9266-4.6591x+12.8147x^2-6.6221x^3)
 %制作：王子兴
 %All rights reserved
